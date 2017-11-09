32b4d8d75b13a16075162695c931b1c06a82b043
samgabriel <sgabriel@brainfuse.com>
2017-11-09 16:37:38 -0500

    Fixed parallel compilation issues of tags and include files
    Added a new subclass of jspc to allow context sharing during parallel
    compilation.

---
+++ src/main/java/io/leonard/maven/plugins/jspc/JspCContextAccessor.java
@@ -0,0 +1,61 @@
+package io.leonard.maven.plugins.jspc;
+
+import java.io.IOException;
+
+import org.apache.jasper.JasperException;
+import org.apache.jasper.JspC;
+import org.apache.jasper.compiler.JspConfig;
+import org.apache.jasper.compiler.JspRuntimeContext;
+import org.apache.jasper.compiler.TagPluginManager;
+import org.apache.jasper.compiler.TldCache;
+import org.apache.jasper.servlet.JspCServletContext;
+import org.xml.sax.SAXException;
+
+public class JspCContextAccessor extends JspC {
+
+	public JspCContextAccessor() {
+		super();
+	}
+	protected void initServletContext() throws JasperException, IOException {
+		initServletContext(this.loader);
+	}
+	
+	@Override
+	protected void initServletContext(ClassLoader classLoader)
+			throws IOException, JasperException {
+		super.initServletContext(classLoader);
+	}
+
+	@Override
+	protected ClassLoader initClassLoader() throws IOException {
+		return super.initClassLoader();
+	}
+	
+	protected JspCServletContext getContext() {
+		return super.context;
+	}
+	
+	protected ClassLoader getLoader() {
+		return this.loader;
+	}
+	
+	protected void setContext(JspCServletContext context) throws IOException, JasperException {
+		this.context = context;
+		
+
+
+        initTldScanner(context, getLoader());
+
+        try {
+            scanner.scan();
+        } catch (SAXException e) {
+            throw new JasperException(e);
+        }
+        tldCache = (TldCache) context.getAttribute(TldCache.SERVLET_CONTEXT_ATTRIBUTE_NAME);
+        rctxt = new JspRuntimeContext(context, this);
+        jspConfig = new JspConfig(context);
+        tagPluginManager = new TagPluginManager(context);
+	}
+	
+
+}
