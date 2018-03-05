96819bf1a4d563057a784f4ba71c8e42be8352ce
Thomas Collignon <tomiphone3G@gmail.com>
2018-03-05 21:20:21 +0100

    add test case with small project with jar in lib and that contains space in one directory

--- src/test/java/io/leonard/maven/plugins/jspc/TestJspcMojo.java
+++ src/test/java/io/leonard/maven/plugins/jspc/TestJspcMojo.java
@@ -47,4 +47,17 @@ public class TestJspcMojo {
     byte[] expectedWebFrag = Files.readAllBytes(expectedWebfrag);
     assertThat(actualWebfrag).isEqualTo(expectedWebFrag);
   }
+  
+  @Test
+  public void should_return_one_compiled_jsp_when_executeMojo_on_project_one_jsp_with_space_with_no_options() throws Exception {
+    // Given
+    File oneJspProject = new File("target/test-classes/unit/project_one_jsp with space");
+
+    // When
+    rule.executeMojo(oneJspProject, "compile");
+
+    // Then
+    Path indexJspPath = Paths.get("target/test-classes/unit/project_one_jsp with space/target/classes/jsp/jsp/index_jsp.class");
+    assertThat(indexJspPath).isNotNull();
+  }
 }
