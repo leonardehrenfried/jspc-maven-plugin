9f29eba09c4d1aa814f434fa3c67e402ec9d6b57
Leonard Ehrenfried <mail@leonard.io>
2023-02-01 23:40:04 +0100

    Update Jakarta dependency

--- src/main/java/io/leonard/maven/plugins/jspc/JspcMojo.java
+++ src/main/java/io/leonard/maven/plugins/jspc/JspcMojo.java
@@ -14,26 +14,54 @@
 //========================================================================
 package io.leonard.maven.plugins.jspc;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
-import java.util.*;
-import java.util.concurrent.*;
-
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import javax.xml.XMLConstants;
-import javax.xml.parsers.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.*;
-
-import org.apache.jasper.*;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import org.apache.jasper.JasperException;
+import org.apache.jasper.JspC;
+import org.apache.jasper.TrimSpacesOption;
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.plugin.*;
-import org.apache.maven.plugins.annotations.*;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.util.*;
+import org.codehaus.plexus.util.DirectoryScanner;
+import org.codehaus.plexus.util.StringUtils;
 import org.eclipse.jdt.internal.compiler.env.NameEnvironmentAnswer;
 import org.xml.sax.SAXException;
 
@@ -284,9 +312,9 @@ public class JspcMojo extends AbstractMojo {
    */
   @Parameter(defaultValue = "true")
   private boolean strictQuoteEscaping;
-  
+
   /**
-   * Set this to 'true' to bypass compilation of JSP sources. 
+   * Set this to 'true' to bypass compilation of JSP sources.
    */
   @Parameter(defaultValue = "false", property = "jspc.skip")
   private boolean skip;
@@ -324,7 +352,7 @@ public class JspcMojo extends AbstractMojo {
     if ( skip ) {
       getLog().info( "Not compiling jsp sources" );
       return;
-    }    
+    }
     try {
       long start = System.currentTimeMillis();
 
