141488b3abbc3099527898471f1fd16bb0329785
Joao Silva <30354367+jpmsilva@users.noreply.github.com>
2024-02-14 11:47:09 +0000

    Allow all trimSpaces options, and expose jar scan filter options. Also added support for redirecting JspC logs to the maven logger.

--- src/main/java/io/leonard/maven/plugins/jspc/JspCContextAccessor.java
+++ src/main/java/io/leonard/maven/plugins/jspc/JspCContextAccessor.java
@@ -2,16 +2,26 @@ package io.leonard.maven.plugins.jspc;
 
 import java.io.IOException;
 import java.util.Map;
-
-import org.apache.jasper.*;
-import org.apache.jasper.compiler.*;
+import org.apache.jasper.JasperException;
+import org.apache.jasper.JspC;
+import org.apache.jasper.compiler.JspConfig;
+import org.apache.jasper.compiler.TldCache;
 import org.apache.jasper.servlet.JspCServletContext;
+import org.apache.tomcat.JarScanner;
+import org.apache.tomcat.util.scan.StandardJarScanFilter;
+import org.apache.tomcat.util.scan.StandardJarScanner;
 import org.eclipse.jdt.internal.compiler.env.NameEnvironmentAnswer;
 
 public class JspCContextAccessor extends JspC {
 
   private Map<String, NameEnvironmentAnswer> resourcesCache;
   private String compilerClass;
+
+  private String tldSkip;
+
+  private String tldScan;
+
+  private Boolean defaultTldScan;
 
   public JspCContextAccessor() {
     super();
@@ -50,6 +60,18 @@ public class JspCContextAccessor extends JspC {
     this.compilerClass = compilerClass;
   }
 
+  public void setTldSkip(String tldSkip) {
+    this.tldSkip = tldSkip;
+  }
+
+  public void setTldScan(String tldScan) {
+    this.tldScan = tldScan;
+  }
+
+  public void setDefaultTldScan(Boolean defaultTldScan) {
+    this.defaultTldScan = defaultTldScan;
+  }
+
   protected void initContext(JspCContextAccessor topJspC) {
     this.context = topJspC.context;
     scanner = topJspC.scanner;
@@ -64,5 +86,26 @@ public class JspCContextAccessor extends JspC {
   @Override
   public String getCompilerClassName() {
     return getcompilerClass();
+  }
+
+  @Override
+  protected void initTldScanner(JspCServletContext context, ClassLoader classLoader) {
+    if (tldSkip != null || tldScan != null || defaultTldScan != null) {
+      JarScanner scanner = new StandardJarScanner();
+      StandardJarScanFilter filter = new StandardJarScanFilter();
+      if (tldSkip != null) {
+        filter.setTldSkip(tldSkip);
+      }
+      if (tldScan != null) {
+        filter.setTldScan(tldScan);
+      }
+      if (defaultTldScan != null) {
+        filter.setDefaultTldScan(defaultTldScan);
+      }
+      scanner.setJarScanFilter(filter);
+      // As seen in org.apache.jasper.compiler.JarScannerFactory.getJarScanner
+      context.setAttribute(JarScanner.class.getName(), scanner);
+    }
+    super.initTldScanner(context, classLoader);
   }
 }
